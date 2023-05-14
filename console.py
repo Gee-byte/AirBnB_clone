4d4eace87bc137e53704a204564ce30fae709ceb
HeseltineBTutu <heseltinetutu@gmail.com>
2023-05-14 05:29:24 -0700

    fix

--- console.py
+++ console.py
@@ -56,6 +56,7 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+<<<<<<< HEAD
     def help_EOF(self):
         """Display information about EOF signal handling."""
         print("EOF signal to exit the program")
@@ -63,6 +64,186 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line"""
         pass
+=======
+    def do_create(self, line):
+        """Create command to create a new instance of a class"""
+        args = line.split()
+        if not args:
+            print("** iclass name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in self.allowed_classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = BaseModel()
+        storage.new(new_instance)
+        storage.save()
+        print(new_instance.id)
+
+    def do_show(self, line):
+        """Show command to print the string representation of an instance"""
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in self.allowed_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + '.' + instance_id
+        all_instances = storage.all()
+        if key not in all_instances:
+            print("** no instance found **")
+            return
+
+        instance = all_instances[key]
+        print(instance)
+
+    def do_destroy(self, line):
+        """Destroy command to delete an instance"""
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in self.allowed_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = class_name + '.' + instance_id
+        all_instances = storage.all()
+        if key not in all_instances:
+            print("** no instance found **")
+            return
+
+        del all_instances[key]
+        storage.save()
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances based or
+        not on the class name.
+        Usage:
+            all             Prints all string representaion of all instances
+                            from all classes.
+
+            <class name>.all() Prints all string representaion of all instances
+                                from the given class name.
+        """
+        args = line.split()
+        all_instances = storage.all()
+
+        if not args:
+            print([str(key) for key in all_instances.values()])
+            return
+        else:
+            try:
+                class_name, method_name = line.split('.')
+                if method_name != 'all()':
+                    raise ValueError
+                cls = models.classes[class_name]
+
+                if args[0] not in self.allowed_classes:
+                    print("** class doesn't exist **")
+                return
+            except (ValueError, KeyError):
+                print("** Unknown syntax:", line)
+                return
+
+            print([str(obj) for obj in all_instances.all(cls).values()])
+
+    def do_update(self, line):
+        """Update command to update an instance"""
+        args = line.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in self.allowed_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        key = None
+        value = None
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        key = args[2]
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        value_str = args[3]
+        try:
+            value = int(value_str)
+        except ValueError:
+            if value_str.startswith('"') and value_str.endswith('"'):
+                value = value_str[1:-1]
+            else:
+                print("** value missing **")
+                return
+
+        objs = storage.all()
+        key_to_update = class_name + "." + obj_id
+        if key_to_update not in objs:
+            print("** no instance found **")
+            return
+
+        obj = objs[key_to_update]
+        setattr(obj, key, value)
+        obj.save()
+
+    def default(self, line):
+        """
+        Called on an input line when the command prefix is not recognized.
+        """
+        parts = line.split(".")
+        if len(parts) == 2 and parts[1] == "all()":
+            # Retrieve all instances of the specified class
+            class_name = parts[0]
+            if class_name not in self.allowed_classes:
+                print("** class doesn't exist **")
+                return
+            all_instances = storage.all()
+            instances = [str(obj) for obj in all_instances.values()
+                         if type(obj).__name__ == class_name]
+            print(instances)
+        elif len(parts) == 2 and parts[1] == "count()":
+            # Retrieve the count of all instances of the specified class
+            class_name = parts[0]
+            if class_name not in self.allowed_classes:
+                print("** class doesn't exist **")
+                return
+            all_instances = storage.all()
+            count = sum(1 for obj in all_instances.values()
+                        if type(obj).__name__ == class_name)
+            print(count)
+        else:
+            print("*** Unknown syntax: {}".format(line))
+>>>>>>> parent of 9509840... added a function to handle empty line  and do nothing
 
 
 if __name__ == '__main__':
